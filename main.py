2faa81a96b8ded43f857dcb56544cee61f8bbe56
GMT-Mati <75912725+GMT-Mati@users.noreply.github.com>
2021-05-19 21:59:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,40 @@
-index = []
+import networkx as nx  # biblioteka do obliczania cykli
 
+
+# wczytanie pliku
 f = open('zadanie_B/slo1ocen.in')
-n = int(f.readline())
 
-wagi = list(f.readline().split(' '))
+n = int(f.readline())  # liczba słoni
+
+waga = list(f.readline().split(' '))  # lista wag słoni, str zamienione na int
 for _ in range(0, len(wagi)):
     wagi[_] = int(wagi[_])
 
+index = []  # index dla wag
+for idx in range(len(waga)):
+    index.append('%i' %(idx + 1))
 
-for r in range(len(wagi)):
-    index.append('%i' %(r + 1))
+dict_waga = dict(zip(index, waga))  # słownik przypisujący konkretną wagę do indexu
 
+start = list(f.readline().split((' ')))  # początkowa kolejność słoni 
+start = list(map(lambda x: x.strip('\n'), start))  # oczyszczenie danych z niepotrzebnych znaków
 
-wag = dict(zip(index, wagi))
+end = list(f.readline().split(' '))  # lista końcowa zaproponowana przez dyrektora zoo
+end = list(map(lambda x: x.strip('\n'), end))
 
+edges = list(zip(start, end)) # wyznaczenie wspólnych krawędzi dla obu list
 
-org = list(f.readline().split((' ')))
-org = list(map(lambda x: x.strip('\n'), org))
+G = nx.DiGraph(edges)  # model krawędzi 
 
-
-fin = list(f.readline().split(' '))
-fin = list(map(lambda x: x.strip('\n'), fin))
-
-
-
-
-import networkx as nx
-
-edges = list(zip(org, fin))
-all_work = 0
-
-G = nx.DiGraph(edges)
-
-for cycle in nx.simple_cycles(G):
+for cycle in nx.simple_cycles(G):  # iteracja przez cykle
     cycle
     
-    cycle = [wag.get(item, item) for item in cycle]
+    cycle = [dict_waga.get(item, item) for item in cycle]  # przypisanie konkretnej wagi do danego imdeksu
     cycle = list(map(int, cycle))
-
+    
+    # praca wykonana przy przesuwaniu słoni dla metody 1 i 2 w pojedyńczym cyklu
     praca1 = sum(item for item in cycle) + (len(cycle) - 2) * (min(cycle))
-    praca2 = sum(item for item in cycle) + min(cycle) + (len(cycle) + 1) * min(wagi) 
-    all_work += min(praca1, praca2) 
+    praca2 = sum(item for item in cycle) + min(cycle) + (len(cycle) + 1) * min(waga) 
+    all_work += min(praca1, praca2)  # suma minimalnej pracy wykonanej przez wszystkie cykle
 
 print(all_work)
-
-
-
-
-
-
-# print()
-# cyk = list(nx.simple_cycles(G))
-# print(cyk)
